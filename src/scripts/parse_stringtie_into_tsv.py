fb557d3ceed7205ad573afadcd5dfb69b7e960ce
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2022-02-13 16:11:19 +0800

    Small bugs fixed

--- src/scripts/parse_stringtie_into_tsv.py
+++ src/scripts/parse_stringtie_into_tsv.py
@@ -2,9 +2,8 @@ import argparse
 import sys
 from typing import List
 
-from bioutils.datastructure import GeneView
-
-from bioutils.datastructure.gene_typing import Transcript
+from bioutils.datastructure.gff_gtf_record import GtfRecord
+from bioutils.io.feature import GtfIterator
 from commonutils import ioctl
 
 __version__ = 0.1
@@ -32,23 +31,24 @@ def _parse_args(args: List[str]) -> argparse.Namespace:
     return parser.parse_args(args)
 
 # FIXME
-def get_with_defaults(transcript: Transcript, key: str) -> str:
-    if key in transcript.data.attribute.keys():
-        return repr(transcript.data.attribute[key])
+def get_with_defaults(transcript_gtf_record:GtfRecord, key: str) -> str:
+    if key in transcript_gtf_record.attribute.keys():
+        return repr(transcript_gtf_record.attribute[key])
     else:
         return "NA"
 
 
 def main(args: List[str]):
     args = _parse_args(args)
-    ans_gv = GeneView.from_file(args.gtf)
+    ans_gv = GtfIterator(args.gtf)
     with ioctl.get_writer(args.out) as writer:
         writer.write("\t".join(POSSIBLE_KEYS) + "\n")
-        for transcript in ans_gv.transcripts.values():
-            current_line = []
-            for key in POSSIBLE_KEYS:
-                current_line.append(get_with_defaults(transcript, key))
-            writer.write("\t".join(current_line) + "\n")
+        for gtf_record in ans_gv:
+            if gtf_record.feature == "transcript":
+                current_line = []
+                for key in POSSIBLE_KEYS:
+                    current_line.append(get_with_defaults(gtf_record, key))
+                writer.write("\t".join(current_line) + "\n")
 
 
 if __name__ == "__main__":
