07677ca6dc81ef1ff7c776dece00a4d1b142837e
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2022-04-01 14:23:24 +0800

    Shell small bugs fixed

--- suppl_data_analysis/h_sapiens/procedure_tgs_single_end.sh
+++ suppl_data_analysis/h_sapiens/procedure_tgs_single_end.sh
@@ -19,26 +19,26 @@ else
     errh "No FASTQ found!"
 fi
 
-[ ! -f "${FASTQ_NAME}".GENE.bam ] && minimap2 -t "${THREAD_NUM}" -a -x splice "${GENE_REFERENCE}" "${FASTQ_NAME}" | \
-samtools sort - -@ "${THREAD_NUM}" -o "${FASTQ_NAME}".GENE.bam
+[ ! -f "${FASTQ_BASE_NAME}".GENE.bam ] && minimap2 -t "${THREAD_NUM}" -a -x splice "${GENE_REFERENCE}" "${FASTQ_NAME}" | \
+samtools sort - -@ "${THREAD_NUM}" -o "${FASTQ_BASE_NAME}".GENE.bam
 
-[ ! -f "${FASTQ_NAME}".TRANS.bam ] && minimap2 -t "${THREAD_NUM}" -a "${TRANSCRIPT_REFERENCE}" "${FASTQ_NAME}" | \
-samtools sort - -@ "${THREAD_NUM}" -o "${FASTQ_NAME}".TRANS.bam
+[ ! -f "${FASTQ_BASE_NAME}".TRANS.bam ] && minimap2 -t "${THREAD_NUM}" -a "${TRANSCRIPT_REFERENCE}" "${FASTQ_NAME}" | \
+samtools sort - -@ "${THREAD_NUM}" -o "${FASTQ_BASE_NAME}".TRANS.bam
 
-[ ! -f "${FASTQ_NAME}".GENE.bam.bai ] && samtools index "${FASTQ_NAME}".GENE.bam
-[ ! -f "${FASTQ_NAME}".TRANS.bam.bai ] && samtools index "${FASTQ_NAME}".TRANS.bam
-[ ! -f "${FASTQ_NAME}".TRANS.bam.depth.tsv ] && samtools depth "${FASTQ_NAME}".TRANS.bam > "${FASTQ_NAME}".TRANS.bam.depth.tsv
+[ ! -f "${FASTQ_BASE_NAME}".GENE.bam.bai ] && samtools index "${FASTQ_BASE_NAME}".GENE.bam
+[ ! -f "${FASTQ_BASE_NAME}".TRANS.bam.bai ] && samtools index "${FASTQ_BASE_NAME}".TRANS.bam
+[ ! -f "${FASTQ_BASE_NAME}".TRANS.bam.depth.tsv ] && samtools depth "${FASTQ_BASE_NAME}".TRANS.bam > "${FASTQ_BASE_NAME}".TRANS.bam.depth.tsv
 
-[ ! -f "${FASTQ_NAME}".transcript.depth.tsv ] &&  \
+[ ! -f "${FASTQ_BASE_NAME}".transcript.depth.tsv ] &&  \
     Rscript R/transform_depth_results.R \
     --libfile R/lib.R \
-    --input "${FASTQ_NAME}".TRANS.bam.depth.tsv \
+    --input "${FASTQ_BASE_NAME}".TRANS.bam.depth.tsv \
     --fa_stats "${TRANSCRIPT_REFERENCE}.stats" \
-    --output "${FASTQ_NAME}".transcript.depth.tsv
-[ ! -f "${FASTQ_NAME}".stringtie_unguided.gtf ] && \
-stringtie "${FASTQ_NAME}".GENE.bam -o "${FASTQ_NAME}".stringtie_unguided.gtf
+    --output "${FASTQ_BASE_NAME}".transcript.depth.tsv
+[ ! -f "${FASTQ_BASE_NAME}".stringtie_unguided.gtf ] && \
+stringtie "${FASTQ_BASE_NAME}".GENE.bam -o "${FASTQ_BASE_NAME}".stringtie_unguided.gtf
 
-[ ! -f "${FASTQ_NAME}".stringtie_guided.gtf ] && \
-stringtie "${FASTQ_NAME}".GENE.bam \
+[ ! -f "${FASTQ_BASE_NAME}".stringtie_guided.gtf ] && \
+stringtie "${FASTQ_BASE_NAME}".GENE.bam \
 -G "${GENE_GTF}" \
--o "${FASTQ_NAME}".stringtie_guided.gtf
+-o "${FASTQ_BASE_NAME}".stringtie_guided.gtf
