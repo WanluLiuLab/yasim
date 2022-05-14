8ac7126445ec9a3cf298b0b2421f73cd21ccec63
yuzj <theafamily@126.com>
2022-05-14 15:46:58 +0800

    GMM integrated. Bugs: After filtering < 0 data, the mean was higher than expected.

--- src/yasim/helper/depth.py
+++ src/yasim/helper/depth.py
@@ -1,7 +1,6 @@
 """
 depth.py -- DGE Datastructure and Utils
 """
-
 from typing import Dict
 
 from scipy.stats import nbinom, uniform
@@ -10,6 +9,7 @@ from bioutils.datastructure.gene_view import GeneViewType
 from commonutils import shell_utils
 from commonutils.importer.tqdm_importer import tqdm
 from commonutils.io.safe_io import get_writer, get_reader
+from yasim.helper.gmm import GaussianMixture1D
 
 DepthType = Dict[str, int]
 """DGE type, is transcript_id -> coverage"""
@@ -50,7 +50,35 @@ def simulate_dge_nb(
     depth = {}
 
     for transcript_id in tqdm(iterable=transcript_ids, desc="Simulating..."):
-        d = int(nb_generator.rvs() / nb_generator_mean * mu)
+        d = 0
+        while not d > 0:
+            d = int(nb_generator.rvs() / nb_generator_mean * mu)
+        depth[transcript_id] = d
+    return depth
+
+
+gmm_model = GaussianMixture1D.import_model(
+    (
+        (0.7533708725243431, 13.50517241736218, 15.935138269740976),
+        (0.24662912747565696, 448.39584422522495, 997.3253918151006)
+    )
+)
+
+
+def simulate_dge_gmm(
+        gv: GeneViewType,
+        mu: int
+) -> DepthType:
+    """
+    Simulate DGE using Gaussian mixture model.
+    """
+    transcript_ids = gv.iter_transcript_ids()
+    depth = {}
+    gmm_model.lintrans(mu / gmm_model.mean())
+    for transcript_id in tqdm(iterable=transcript_ids, desc="Simulating..."):
+        d = 0
+        while not d > 0:
+            d = int(gmm_model.rvs())
         depth[transcript_id] = d
     return depth
 
--- suppl_data_analysis/batch_c_elegans/fit_gaussian_mixture_final.py
+++ suppl_data_analysis/batch_c_elegans/fit_gaussian_mixture_final.py
@@ -4,138 +4,19 @@ Fit Gaussian Mixture by YUAN Ruihong
 
 import argparse
 import sys
-from typing import Optional, Union
 from random import random
 
 import numpy as np
-from numpy.typing import ArrayLike
-from numpy.random import choice
 import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib.ticker import PercentFormatter
 import pandas as pd
 from scipy.stats import norm
 from sklearn.preprocessing import scale
 
+from yasim.helper.gmm import GaussianMixture1D
+
 matplotlib.use('qtagg')
 
-class GaussianMixture1D:
-    _n_components: int
-    _n_iter: int
-    _precision: float
-    _weights: ArrayLike
-    _mu: ArrayLike
-    _sigma: ArrayLike
-
-    def __init__(
-            self, n_components: int = 1,
-            n_iter: int = 100,
-            init_weights: Optional[ArrayLike] = None,
-            init_mus: Optional[ArrayLike] = None,
-            init_sigmas: Optional[ArrayLike] = None,
-            precision: float = 1E-4,
-    ) -> None:
-        self._n_components = n_components
-        self._n_iter = n_iter
-        self._precision = precision
-        self._weights = init_weights or np.repeat(1/n_components, n_components)
-        self._mu = init_mus or np.zeros(n_components)
-        self._sigma = init_sigmas or np.ones(n_components)
-
-    def fit(self, data: ArrayLike):
-        N = len(data)
-
-        # Use k-means to cluster points
-        centers = choice(data, size=self._n_components)
-        categories = np.repeat(0, N)
-        bags = [[]] * self._n_components
-        for _ in range(self._n_iter):
-            converged = True
-            for j in range(self._n_components):
-                bags[j] = []
-
-            for i in range(N):
-                x = data[i]
-                category = categories[i]
-                least_dist = abs(x - centers[category])
-                for j in range(self._n_components):
-                    center = centers[j]
-                    dist = abs(x - center)
-                    if dist < least_dist:
-                        converged = False
-                        category = j  # assign to nearest center
-                        least_dist = dist
-                categories[i] = category
-                bags[category].append(x)
-
-            for j in range(self._n_components):
-                bag = np.array(bags[j])
-                bags[j] = bag
-                centers[j] = np.mean(bag)
-
-            if converged:
-                break
-
-        self._mu = centers
-        for j in range(self._n_components):
-            self._sigma[j] = np.std(bags[j])
-            self._weights[j] = len(bags[j])
-        self._weights /= N
-
-        last_ll = np.NINF
-        densities = np.zeros((self._n_components, N))
-        for epoch in range(self._n_iter):
-            # E step: compute ownership weights
-            for j in range(self._n_components):
-                model = norm(loc=self._mu[j], scale=self._sigma[j])
-                densities[j, :] = model.pdf(data)
-
-            a = np.transpose(np.multiply(np.transpose(densities), self._weights))
-            b = (np.transpose(self._weights) @ densities)
-            gamma = a / b
-
-            # M step: compute means, variances and weights
-            denom = np.sum(gamma, axis=1)  # row sum
-            self._mu = (gamma @ data) / denom
-
-            for j in range(self._n_components):
-                for i in range(N):
-                    gamma[j, i] *= (data[i] - self._mu[j])**2
-
-            self._sigma = np.sqrt(np.sum(gamma, axis=1) / denom)
-            self._weights = denom / N
-
-            ll = self.log_likelihood(data)
-            if 0 <= ll - last_ll < self._precision:
-                break
-            last_ll = ll
-
-        return self
-
-    def log_likelihood(self, data):
-        return np.sum(self.logpdf(data))
-
-    def pdf(self, x: Union[int, float, ArrayLike]) -> float:
-        if isinstance(x, (int, float)):
-            result = 0.0
-        else:
-            result = np.zeros(len(x))
-
-        for j in range(self._n_components):
-            model = norm(loc=self._mu[j], scale=self._sigma[j])
-            result += self._weights[j] * model.pdf(x)
-        return result
-
-    def logpdf(self, x: Union[int, float, ArrayLike]) -> float:
-        if isinstance(x, (int, float)):
-            result = 0.0
-        else:
-            result = np.zeros(len(x))
-
-        for j in range(self._n_components):
-            model = norm(loc=self._mu[j], scale=self._sigma[j])
-            result += self._weights[j] * model.logpdf(x)
-        return result
 
 def _main():
     parser = argparse.ArgumentParser()
@@ -170,20 +51,16 @@ def _main():
     model = GaussianMixture1D(n_components=args.numComponents,
                               n_iter=args.numIters).fit(data)
     print(f"Fitted to Gaussian mixture model ({args.numComponents} components)")
-    model_desc = "Model ="
-    for j in range(model._n_components):
-        model_desc += f" + {model._weights[j]}*N({model._mu[j]}, {model._sigma[j]})"
-    print(model_desc)
+    print(str(model))
 
-    fig, axes = plt.subplots(1, 2)
-    axes[0].yaxis.set_major_formatter(PercentFormatter(xmax=1))
+    fig, axes = plt.subplots(1, 1)
 
     x_max = np.max(data)
     n_bins = min(40, int(x_max))
     xticks = np.linspace(np.min(data), x_max, 100)
-
-    axes[0].hist(data, bins=n_bins, density=True)
-    axes[1].plot(xticks, model.pdf(xticks))
+    axes.hist(data, bins=n_bins, density=True)
+    axes.plot(xticks, model.pdf(xticks), color="red")
+    # plt.yscale('log')
     plt.show()
 
 if __name__ == '__main__':
