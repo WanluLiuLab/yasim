7344c3e6a78f5e05b53455ad3e7258813bd8263f
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2023-02-13 15:22:13 +0800

    Small bugs in depth simulation fixed

---
+++ suppl_data_analysis/syq_article_simulation/plot_nipg.R
@@ -0,0 +1,156 @@
+library("tidyverse")
+library("pheatmap")
+
+fns <- Sys.glob("ce11_as_*.gtf.gz.gene.tsv.xz")
+conditions <- fns %>%
+    stringr::str_replace("ce11_as_", "") %>%
+    stringr::str_replace(".gtf.gz.gene.tsv.xz", "")
+
+fns <- c(
+    "ce11.ncbiRefSeq.gtf.gz.gene.tsv.xz",
+    fns
+)
+
+conditions <- c(
+    "REFERENCE",
+    conditions
+)
+
+if (file.exists("all_nipg_data_binned.parquet")){
+    all_nipg_data_binned <- arrow::read_parquet("all_nipg_data_binned.parquet")
+} else{
+    #' All NIpG Data
+    all_nipg_data <- list()
+
+    #' Read data
+    for (i in seq_along(conditions)) {
+        all_nipg_data[[conditions[i]]] <-
+            readr::read_tsv(
+                fns[i],
+                show_col_types = FALSE,
+                col_types = c(
+                    GENE_ID = col_character(),
+                    TRANSCRIPT_NUMBER = col_integer(),
+                    NAIVE_LENGTH = col_integer(),
+                    TRANSCRIBED_LENGTH = col_integer(),
+                    MAPPABLE_LENGTH = col_integer()
+                )
+            ) %>%
+                dplyr::select(TRANSCRIPT_NUMBER) %>%
+                unlist() %>%
+                as.vector()
+    }
+
+    n_bins <- 0
+    for (i in conditions) {
+        n_bins <- max(n_bins, max(all_nipg_data[[i]]))
+    }
+
+    all_nipg_data_binned <- tibble(
+        x = 1:n_bins
+    )
+
+    for (i in conditions) {
+        adding_col <- NULL
+        for (n in 1:n_bins) {
+            adding_col[n] <- length(which(all_nipg_data[[i]] == n))
+        }
+        all_nipg_data_binned <- all_nipg_data_binned %>%
+            dplyr::mutate(!!i := adding_col)
+    }
+    arrow::write_parquet(all_nipg_data_binned, "all_nipg_data_binned.parquet")
+}
+
+pheatmap_data <- all_nipg_data_binned
+pheatmap_data$x <- NULL
+
+pheatmap(
+    log(pheatmap_data + 1),
+    cluster_rows = FALSE,
+    cluster_cols = FALSE,
+    main = "Logged Number of isoforms in a gene accross all conditions",
+    filename = "nipg_heatmap.pdf",
+    width = 8,
+    height = 5
+)
+
+all_nipg_data_binned_long <- all_nipg_data_binned %>%
+    tidyr::gather(key="Condition", value="Count", -x)
+
+g <- ggplot(all_nipg_data_binned_long) +
+    geom_bar(aes(x=x, y=Count), stat="identity") +
+    scale_y_continuous(trans="log10") +
+    theme_bw() +
+    facet_wrap(.~Condition)
+ggsave("nipg_hist.pdf", g, width=8, height=5)
+
+#' Count number of genes and transcripts in each sample
+
+fns <- Sys.glob("ce11_as_*.gtf.gz.transcripts.tsv.xz")
+conditions <- fns %>%
+    stringr::str_replace("ce11_as_", "") %>%
+    stringr::str_replace(".gtf.gz.transcripts.tsv.xz", "")
+
+fns <- c(
+    "ce11.ncbiRefSeq.gtf.gz.transcripts.tsv.xz",
+    fns
+)
+
+conditions <- c(
+    "REFERENCE",
+    conditions
+)
+
+
+if (file.exists("all_nipg_ngenes_nisoforms.parquet")){
+    all_nipg_ngenes_nisoforms <- arrow::read_parquet("all_nipg_ngenes_nisoforms.parquet")
+} else{
+    #' All NIpG Data
+    all_data <- NULL
+
+    #' Read data
+    for (i in seq_along(conditions)) {
+        this_data <- readr::read_tsv(
+                fns[i],
+                show_col_types = FALSE,
+                col_types = c(
+                    TRANSCRIPT_ID = col_character(),
+                    GENE_ID = col_character(),
+                    TRANSCRIPT_NUMBER = col_integer(),
+                    NAIVE_LENGTH = col_integer(),
+                    TRANSCRIBED_LENGTH = col_integer(),
+                    EXON_NUMBER = col_integer()
+                )
+            ) %>%
+                dplyr::select(TRANSCRIPT_ID, GENE_ID) %>%
+                dplyr::mutate(Condition=conditions[i])
+        if (is.null(all_data)){
+            all_data <- this_data
+        } else{
+            all_data <- all_data %>%
+                dplyr::rows_append(this_data)
+        }
+    }
+    all_nipg_ngenes_nisoforms <- all_data %>%
+        dplyr::group_by(Condition) %>%
+        dplyr::transmute(
+            nGenes=length(unique(GENE_ID)),
+            nIsoforms=length(unique(TRANSCRIPT_ID)),
+            Condition=Condition
+        ) %>%
+        dplyr::distinct()
+
+    arrow::write_parquet(all_nipg_ngenes_nisoforms, "all_nipg_ngenes_nisoforms.parquet")
+}
+
+g <- ggplot(all_nipg_ngenes_nisoforms) +
+    geom_bar(aes(y=Condition, x=nGenes), stat="identity") +
+    theme_bw()
+
+ggsave("nipg_ngenes.pdf", g, width=8, height=5)
+
+g <- ggplot(all_nipg_ngenes_nisoforms) +
+    geom_bar(aes(y=Condition, x=nIsoforms), stat="identity") +
+    theme_bw()
+
+ggsave("nipg_nisoforms.pdf", g, width=8, height=5)
--- suppl_data_analysis/syq_article_simulation/run.sh
+++ suppl_data_analysis/syq_article_simulation/run.sh
@@ -25,7 +25,7 @@ function perform_simulation(){
         -e ../bin/pbsim3 \
         -m RSII \
         -M qshmm \
-        -F ../ce11_trans_2.fa.gz \
+        -F ../ce11_trans_2.fa.d \
         -d "${1}".tsv.xz \
         -o "${2}"_pbsim3_RSII_CLR \
         -j 40
@@ -33,33 +33,33 @@ function perform_simulation(){
         -e ../bin/pbsim3 \
         -m RSII \
         -M qshmm \
-        -F ../ce11_trans_2.fa.gz \
+        -F ../ce11_trans_2.fa.d \
         --ccs_pass 10 \
         -d "${1}".tsv.xz \
         -o "${2}"_pbsim3_RSII_CCS \
         -j 40
     python -m yasim pbsim3 \
         -e ../bin/pbsim3 \
-        -m Sequel \
+        -m SEQUEL \
         -M errhmm \
-        -F ../ce11_trans_2.fa.gz \
+        -F ../ce11_trans_2.fa.d \
         -d "${1}".tsv.xz \
-        -o "${2}"_pbsim3_Sequel_CLR \
+        -o "${2}"_pbsim3_SEQUEL_CLR \
         -j 40
     python -m yasim pbsim3 \
         -e ../bin/pbsim3 \
-        -m Sequel \
+        -m SEQUEL \
         -M errhmm \
-        -F ../ce11_trans_2.fa.gz \
+        -F ../ce11_trans_2.fa.d \
         --ccs_pass 10 \
         -d "${1}".tsv.xz \
-        -o "${2}"_pbsim3_Sequel_CCS \
+        -o "${2}"_pbsim3_SEQUEL_CCS \
         -j 40
     for pbsim2_mode in R94 R103; do
         python -m yasim pbsim2 \
             -e ../bin/pbsim2 \
             -m "${pbsim2_mode}" \
-            -F ../ce11_trans_2.fa.gz \
+            -F ../ce11_trans_2.fa.d \
             -d "${1}".tsv.xz \
             -o "${2}"_pbsim2_"${pbsim2_mode}" \
             -j 40
@@ -92,7 +92,6 @@ cd .. || exit 1
 mkdir -p diff_read_depth
 cd diff_read_depth || exit 1
 
-
 for mean_depth in 10 25 40 55 70 85 100; do
     python -m yasim generate_gene_depth \
         -g ../ce11_as_2.gtf.gz \
@@ -102,8 +101,10 @@ for mean_depth in 10 25 40 55 70 85 100; do
         -g ../ce11_as_2.gtf.gz \
         -d ce11_as_2_gene_depth_"${mean_depth}".tsv.xz \
         -o ce11_as_2_isoform_depth_"${mean_depth}".tsv.xz
-    perform_simulation ce11_as_2_isoform_depth_"${mean_depth}" ce11_as_2_isoform_depth_"${mean_depth}"
+    perform_simulation ce11_as_2_isoform_depth_"${mean_depth}" ce11_as_2_isoform_depth_"${mean_depth}" &
 done
+wait
+
 
 cd .. || exit 1
 
