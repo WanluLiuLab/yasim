eb45a2db96be18163f63d6fb8230e1958be2464e
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2023-02-13 20:40:52 +0800

    Some "unit test"-like code added for LLRGs

---
+++ test/llrg/run.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -ue
+python -m yasim transcribe \
+    -g ../ce11.ncbiRefSeq.chrM.gtf \
+    -f ../chrM.fa \
+    -o chrM.trans.fa
+python -m yasim generate_depth_v2 \
+    -g ../ce11.ncbiRefSeq.chrM.gtf \
+    -o depth.tsv \
+    --mu 10
+
+python -m yasim pbsim3 \
+    -e /mnt/volume2/TGS/phase_2_simulation/yasim-dev/suppl_data_analysis/syq_article_simulation/bin/pbsim3 \
+    -m RSII \
+    -M qshmm \
+    -F chrM.trans.fa.d \
+    -d depth.tsv \
+    -o pbsim3_RSII_CLR \
+    -j 40
+python -m yasim pbsim3 \
+    -e /mnt/volume2/TGS/phase_2_simulation/yasim-dev/suppl_data_analysis/syq_article_simulation/bin/pbsim3 \
+    -m RSII \
+    -M qshmm \
+    -F chrM.trans.fa.d \
+    --ccs_pass 10 \
+    -d depth.tsv \
+    -o pbsim3_RSII_CCS \
+    -j 40
+python -m yasim pbsim3 \
+    -e /mnt/volume2/TGS/phase_2_simulation/yasim-dev/suppl_data_analysis/syq_article_simulation/bin/pbsim3 \
+    -m SEQUEL \
+    -M errhmm \
+    -F chrM.trans.fa.d \
+    -d depth.tsv \
+    -o pbsim3_SEQUEL_CLR \
+    -j 40
+python -m yasim pbsim3 \
+    -e /mnt/volume2/TGS/phase_2_simulation/yasim-dev/suppl_data_analysis/syq_article_simulation/bin/pbsim3 \
+    -m SEQUEL \
+    -M errhmm \
+    -F chrM.trans.fa.d \
+    --ccs_pass 10 \
+    -d depth.tsv \
+    -o pbsim3_SEQUEL_CCS \
+    -j 40
+for pbsim2_mode in R94 R103; do
+    python -m yasim pbsim2 \
+        -e /mnt/volume2/TGS/phase_2_simulation/yasim-dev/suppl_data_analysis/syq_article_simulation/bin/pbsim2 \
+        -m "${pbsim2_mode}" \
+        -F chrM.trans.fa.d \
+        -d depth.tsv \
+        -o pbsim2_"${pbsim2_mode}" \
+        -j 40
+done
