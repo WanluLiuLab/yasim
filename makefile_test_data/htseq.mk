f26df2cfb35ecaa295df4cd7e934705cecf0fa75
yuzj <theafamily@126.com>
2023-02-16 14:23:51 +0800

    Unused Makefiles removed

--- makefile_test_data/htseq.mk
+++
@@ -1,4 +0,0 @@
-$(DATADIR)/htseq_quant_%.tsv:$(DATADIR)/%.bam $(DATADIR)/%.bam.bai $(REFERENCE_GTF)
-	htseq-count --idattr transcript_id -n $(THREADS) -c $@ $< "$(REFERENCE_GTF)"
-$(DATADIR)/htseq_genelevel_quant_%.tsv:$(DATADIR)/%.bam $(DATADIR)/%.bam.bai $(REFERENCE_GTF)
-	htseq-count --idattr gene_id -n $(THREADS) -c $@ $< "$(REFERENCE_GTF)"
