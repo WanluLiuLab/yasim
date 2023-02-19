945b2a067646a733846565d09d2a3995a916b316
yuzj <theafamily@126.com>
2023-02-19 15:29:05 +0800

    Extremes filtering added

--- src/yasim/__init__.py
+++ src/yasim/__init__.py
@@ -4,7 +4,7 @@ yasim -- Yet Another SIMulator for Alternative Splicing and Differentially Expre
 This is an alternative splicing simulator, with interesting functions like DEG simulation.
 """
 
-__version__ = "3.1.3"
+__version__ = "3.1.4"
 __author__ = ",".join((
     "YU Zhejian"
 ))
--- src/yasim/main/generate_gene_depth.py
+++ src/yasim/main/generate_gene_depth.py
@@ -1,13 +1,33 @@
+import argparse
 from typing import List
 
 from labw_utils.bioutils.datastructure.gene_view import GeneViewFactory
 
 from yasim.helper import depth
-from yasim.main.generate_depth_v2 import _parse_args
 
+
+def _parse_args(args: List[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--gtf', required=True, help="Input GTF format", nargs='?',
+                        type=str, action='store')
+    parser.add_argument('-o', '--out', required=True, help="Output TSV", nargs='?',
+                        type=str, action='store')
+    parser.add_argument('-d', '--mu', required=False, help="Average depth", nargs='?',
+                        type=float, action='store', default=20)
+    parser.add_argument('--low_cutoff', required=False, help="Depth lower than this value would be 0.", nargs='?',
+                        type=float, action='store', default=0.01)
+    parser.add_argument('--high_cutoff_ratio', required=False,
+                        help="Depth higher than mu * high_cutoff_ratio would be 0.", nargs='?',
+                        type=float, action='store', default=10)
+    return parser.parse_args(args)
 
 def main(args: List[str]):
     args = _parse_args(args)
     gv = GeneViewFactory.from_file(args.gtf)
-    dge_data = depth.simulate_gene_level_depth_gmm(gv=gv, mu=args.mu)
+    dge_data = depth.simulate_gene_level_depth_gmm(
+        gv=gv,
+        mu=args.mu,
+        low_cutoff=args.low_cutoff,
+        high_cutoff_ratio=args.high_cutoff_ratio
+    )
     depth.write_depth(dge_data, args.out, feature_name="GENE_ID")
