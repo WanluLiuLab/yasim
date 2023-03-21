3cbf133133160ffc256ce70da41d102229974a15
ZJ YU <Zhejian.19@intl.zju.edu.cn>
2023-03-21 12:25:59 +0800

    Bugs fixed at LZZ server

--- suppl_data_analysis/syq_article_simulation/src/run.sh
+++ suppl_data_analysis/syq_article_simulation/src/run.sh
@@ -4,6 +4,7 @@ set -uex
 git clone http://github.com/yukiteruono/pbsim3
 cd pbsim3 || exit 1
 git checkout f3e5f1cf3d0e8346b5e4598ac238b2b570b223e8
+autoreconf -i
 ./configure
 make -j20
 ln -sf "$(pwd)"/src/pbsim ../../bin/pbsim3
@@ -12,6 +13,7 @@ cd .. || exit 1
 git clone http://github.com/yukiteruono/pbsim2
 cd pbsim2 || exit 1
 git checkout eeb5a19420534a0f672c81db2670117e62a9ee38
+autoreconf -i
 ./configure
 make -j20
 ln -sf "$(pwd)"/src/pbsim ../../bin/pbsim2
@@ -19,7 +21,8 @@ cd .. || exit 1
 
 git clone http://github.com/yukiteruono/pbsim
 cd pbsim || exit 1
-git checkout # TODO eeb5a19420534a0f672c81db2670117e62a9ee38
+# git checkout TODO
+autoreconf -i
 ./configure
 make -j20
 ln -sf "$(pwd)"/src/pbsim ../../bin/pbsim
