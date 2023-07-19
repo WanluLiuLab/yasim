ed4e4c79ef97915399e5cecd493afd9db6da9986
yuzj <theafamily@126.com>
2023-07-19 21:13:29 +0800

    syq_article analysis code removed

--- suppl_data_analysis/syq_article_simulation/diff_genome_complexity/p5.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-from yasim.helper import depth_io
-
-if __name__ == "__main__":
-    for fn in glob.glob("ce11_as_*_isoform_depth_20.tsv"):
-        print(fn)
-        out_fn = fn.replace("20", "25")
-        depth_io.write_depth({k: v + 5 for k, v in depth_io.read_depth(fn).items()}, out_fn, "TRANSCRIPT_ID")
